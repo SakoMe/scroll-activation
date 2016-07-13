e5077bf376ece01ec2697c85465eb715eac145bb
Sarkis M <melkonsa@gmail.com>
2016-07-12 17:08:37 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+  var aboutEl = $('div.about'),
+      aboutElOffset = aboutEl.offset().top/2,
+      documentEl = $(document);
+
+  documentEl.on('scroll', function() {
+      if ( documentEl.scrollTop() > aboutElOffset && aboutEl.hasClass('hidden') ) aboutEl.removeClass('hidden');
+  });
+
+});
